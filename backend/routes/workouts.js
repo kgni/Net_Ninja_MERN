c7839cfd935bd2487753117819b3285b88e0b21f
mdiadk <acc@mdia.dk>
2022-06-21 20:38:46 +0200

    add controllers folder, add some controllers for our workout routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,31 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const Workout = require('../models/workoutModel');
+const {
+	getWorkout,
+	getWorkouts,
+	createWorkout,
+} = require('../controllers/workoutController');
 
 // GET all workouts
-router.get('/', (req, res) => {
-	res.json({ msg: 'GET all workouts' });
-});
+router.get('/', getWorkouts);
 
 // GET single workout
-router.get('/:id', (req, res) => {
-	res.json({ msg: `GET a single workout, ID: ${req.params.id}` });
-});
+router.get('/:id', getWorkout);
 
 // POST a new workout
-router.post('/', async (req, res) => {
-	const { title, load, reps } = req.body;
-	try {
-		const workout = await Workout.create({
-			title,
-			load,
-			reps,
-		});
-		res.status(200).json(workout);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
-});
+router.post('/', createWorkout);
 // DELETE a workout
 router.delete('/:id', (req, res) => {
 	res.json({ msg: 'DELETE a workout' });
