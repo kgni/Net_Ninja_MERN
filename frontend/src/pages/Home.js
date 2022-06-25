abcc536836ab5c539fd36dd3ead7252fcd5e71de
mdiadk <acc@mdia.dk>
2022-06-25 14:28:19 +0200

    add GET fetch to backend from react front-end, set proxyfield to get around CORS ERROR, create component for workouts, render every workout in Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+// components
+import WorkoutDetails from '../components/WorkoutDetails';
 
 const Home = () => {
+	const [workouts, setWorkouts] = useState(null);
+	useEffect(() => {
+		const fetchWorkouts = async () => {
+			const response = await fetch('/api/workouts');
+			const data = await response.json();
+
+			if (response.ok) {
+				setWorkouts(data);
+			}
+		};
+		fetchWorkouts();
+	}, []);
 	return (
 		<div className="home">
 			<h2>Home</h2>
+			<div className="workouts">
+				{workouts &&
+					workouts.map((workout) => (
+						<WorkoutDetails key={workout._id} workout={workout} />
+					))}
+			</div>
 		</div>
 	);
 };
